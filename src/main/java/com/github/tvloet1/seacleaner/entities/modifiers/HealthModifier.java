553e4b2b5d44906643971c40f80e8b23ec2c3f4a
tvloet1 <t.vloet1@student.han.nl>
2023-06-16 16:38:14 +0200

    Replaced buffs and nerfs with single modifier for each type

--- src/main/java/com/github/tvloet1/seacleaner/entities/modifiers/HealthModifier.java
+++ src/main/java/com/github/tvloet1/seacleaner/entities/modifiers/HealthModifier.java
@@ -3,11 +3,10 @@ package com.github.tvloet1.seacleaner.entities.modifiers;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.tvloet1.seacleaner.entities.Swimmer;
 
-public class HealthBuff extends Modifier {
-    public HealthBuff(Coordinate2D initialLocation, int modifierValue) {
-        super("sprites/modifiers/healthBuff.png", initialLocation, "audio/soundHeal.mp3", modifierValue);
+public class HealthModifier extends Modifier {
+    public HealthModifier(String resource, Coordinate2D initialLocation, String soundClip, int modifierValue) {
+        super(resource, initialLocation, soundClip, modifierValue);
     }
-
     @Override
     public void modify(Swimmer swimmer) {
         swimmer.adjustHealth(modifierValue);
